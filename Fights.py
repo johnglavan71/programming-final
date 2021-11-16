a2b08758d8c2ac1b69874afcc7a1addb943107b1
John Baker <johnglavan71@gmail.com>
2021-11-16 10:46:17 -0800

    Merge remote-tracking branch 'origin/Characters' into items

--- Fights.py
+++ Fights.py
@@ -1,4 +1,20 @@
-import Monsters
-import Player
+from Monsters import m1
+from Player import p
+from time import sleep
+
 
 def fight1():
+    matk = m1.atk - p.dfn
+    print('Monster does', matk, 'damage.')
+    patk = p.atk - m1.dfn
+    print('You do', patk,'damage.')
+    fight = input('Do you wish to fight? Y/N \n: ')
+    if fight == 'Y':
+        while m1.hp > 0:
+            print(f'You attack the monster.\n You deal {patk} damage.')
+            m1.hp -= patk
+            print(f'Monster has {m1.hp} hp left.')
+            sleep(1)
+
+
+fight1()
--- Monsters.py
+++ Monsters.py
@@ -1,6 +1,6 @@
 
 
-class player:
+class monster:
     # type is the class you choose
     def __init__(self, name, type, hp, dfn, atk, cur):
         self.name = name
@@ -15,9 +15,9 @@ def villagemonster():
     type = 'Rat'
     hp = 15
     dfn = 10
-    atk = 10
+    atk = 15
     cur = 5
-    m1 = (name, type, hp, dfn, atk, cur)
+    m1 = monster(name, type, hp, dfn, atk, cur)
     return m1
 m1 = villagemonster()
 
@@ -29,7 +29,7 @@ def forsetmonster():
     dfn = 5
     atk = 25
     cur = 15
-    m2 = (name, type, hp, dfn, atk, cur)
+    m2 = monster(name, type, hp, dfn, atk, cur)
     return m2
 m2 = forsetmonster()
 
@@ -41,138 +41,138 @@ def ssmonster():
     dfn = 0
     atk = 0
     cur = 0
-    m3 = (name, type, hp, dfn, atk, cur)
+    m3 = monster(name, type, hp, dfn, atk, cur)
     return m3
 m3 = ssmonster()
 
 
 def ddmonster():
-    name =
+    name = '?'
     type = 'Shark'
     hp = 200
     dfn = 15
     atk = 75
     cur = 50
-    m4 = (name, type, hp, dfn, atk, cur)
+    m4 = monster(name, type, hp, dfn, atk, cur)
     return m4
 m4 = ddmonster()
 
 
 def ironmonster():
-    name =
+    name = '?'
     type = 'Kobald'
     hp = 200
     dfn = 20
     atk = 100
     cur = 100
-    m1 = (name, type, hp, dfn, atk, cur)
+    m5 = monster(name, type, hp, dfn, atk, cur)
     return m5
 m5 = ironmonster()
 
 
 def mmmonster():
-    name =
-    type =
+    name = '?'
+    type = '?'
     hp = 75
     dfn = 15
     atk = 10
     cur = 20
-    m6 = (name, type, hp, dfn, atk, cur)
+    m6 = monster(name, type, hp, dfn, atk, cur)
     return m6
 m6 = mmmonster()
 
 
 def gbmonster():
-    name =
-    type =
+    name = '?'
+    type = '?'
     hp = 100
     dfn = 25
     atk = 75
     cur = 75
-    m7 = (name, type, hp, dfn, atk, cur)
+    m7 = monster(name, type, hp, dfn, atk, cur)
     return m7
 m7 = gbmonster()
 
 
 def mazeboss():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b1 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp= '?'
+    dfn= '?'
+    atk= '?'
+    cur= '?'
+    b1 = monster(name, type, hp, dfn, atk, cur)
     return b1
 b1 = mazeboss()
 
 
 def cetusboss():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b2 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp= '?'
+    dfn= '?'
+    atk= '?'
+    cur= '?'
+    b2 = monster(name, type, hp, dfn, atk, cur)
     return b2
 b2 = cetusboss()
 
 
 def genvillan():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b3 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp = '?'
+    dfn = '?'
+    atk = '?'
+    cur = '?'
+    b3 = monster(name, type, hp, dfn, atk, cur)
     return b3
 b3 = genvillan()
 
 
 def persephoneboss():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b4 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp= '?'
+    dfn= '?'
+    atk= '?'
+    cur= '?'
+    b4 = monster(name, type, hp, dfn, atk, cur)
     return b4
 b4 = persephoneboss()
 
 
 def hades():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b5 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp= '?'
+    dfn= '?'
+    atk= '?'
+    cur= '?'
+    b5 = monster(name, type, hp, dfn, atk, cur)
     return b5
 b5 = hades()
 
 
 def thegreatweasel():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b6 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp= '?'
+    dfn= '?'
+    atk= '?'
+    cur= '?'
+    b6 = monster(name, type, hp, dfn, atk, cur)
     return b6
 b6 = thegreatweasel()
 
 
 def mcboss():
-    name=
-    type=
-    hp=
-    dfn=
-    atk=
-    cur=
-    b7 = (name, type, hp, dfn, atk, cur)
+    name= '?'
+    type= '?'
+    hp= '?'
+    dfn= '?'
+    atk= '?'
+    cur= '?'
+    b7 = monster(name, type, hp, dfn, atk, cur)
     return b7
 b7 = mcboss()
