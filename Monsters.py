8d91cce1cbd8b1cb16f4116e2ec89e226f531298
John Baker <johnglavan71@gmail.com>
2021-11-16 09:35:27 -0800

    Merge remote-tracking branch 'origin/Characters' into items

---
+++ Monsters.py
@@ -0,0 +1,178 @@
+
+
+class player:
+    # type is the class you choose
+    def __init__(self, name, type, hp, dfn, atk, cur):
+        self.name = name
+        self.type = type
+        self.hp = hp
+        self.dfn = dfn
+        self.atk = atk
+        self.cur = cur
+
+def villagemonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m1 = (name, type, hp, dfn, atk, cur)
+    return m1
+m1 = villagemonster()
+
+
+def forsetmonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m2 = (name, type, hp, dfn, atk, cur)
+    return m2
+m2 = forsetmonster()
+
+
+def ssmonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m3 = (name, type, hp, dfn, atk, cur)
+    return m3
+m3 = ssmonster()
+
+
+def ddmonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m4 = (name, type, hp, dfn, atk, cur)
+    return m4
+m4 = ddmonster()
+
+
+def ironmonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m1 = (name, type, hp, dfn, atk, cur)
+    return m5
+m5 = ironmonster()
+
+
+def mmmonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m6 = (name, type, hp, dfn, atk, cur)
+    return m6
+m6 = mmmonster()
+
+
+def gbmonster():
+    name =
+    type =
+    hp =
+    dfn =
+    atk =
+    cur =
+    m7 = (name, type, hp, dfn, atk, cur)
+    return m7
+m7 = gbmonster()
+
+
+def mazeboss():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b1 = (name, type, hp, dfn, atk, cur)
+    return b1
+b1 = mazeboss()
+
+
+def cetusboss():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b2 = (name, type, hp, dfn, atk, cur)
+    return b2
+b2 = cetusboss()
+
+
+def genvillan():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b3 = (name, type, hp, dfn, atk, cur)
+    return b3
+b3 = genvillan()
+
+
+def persephoneboss():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b4 = (name, type, hp, dfn, atk, cur)
+    return b4
+b4 = persephoneboss()
+
+
+def hades():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b5 = (name, type, hp, dfn, atk, cur)
+    return b5
+b5 = hades()
+
+
+def thegreatweasel():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b6 = (name, type, hp, dfn, atk, cur)
+    return b6
+b6 = thegreatweasel()
+
+
+def mcboss():
+    name=
+    type=
+    hp=
+    dfn=
+    atk=
+    cur=
+    b7 = (name, type, hp, dfn, atk, cur)
+    return b7
+b7 = mcboss()
