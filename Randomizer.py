d73acc934516d1bc89dc16f4c336b24fcfd97d4a
Christopher Ramirez <cramirezfcm@gmail.com>
2021-11-17 12:07:19 -0800

    item stats updated/ more randomizers

--- Randomizer.py
+++ Randomizer.py
@@ -1,18 +1,36 @@
 import random
+def ran13():
+    ran13 = random.randint(1,3)
+    return ran13
+ran13 = ran13()
+
+def ran15():
+    ran15 = random.randint(1, 6)
+    return ran15
+ran15 = ran15()
 
 def ran110():
-    num = random.randint(1, 10)
-    print(num)
+    ran110 = random.randint(1, 11)
+    return ran110
+ran110 = ran110()
 
 def ran1020():
-    num = random.randint(10, 20)
-    print(num)
+    ran1020 = random.randint(10, 21)
+    return ran1020
+ran1020 = ran1020()
 
 def ran120():
-    num = random.randint(1, 20)
-    print(num)
+    ran120 = random.randint(1, 21)
+    return ran120
+ran120 = ran120()
 
 def ran1100():
-    num = random.randint(1,100)
-    print(num)
+    ran1100 = random.randint(1,101)
+    return ran1100
+ran1100 = ran1100()
+
+def ran1001000():
+    ran1001000 = random.randint(100,1000)
+    return ran1001000
+ran1001000 = ran1001000()
 
